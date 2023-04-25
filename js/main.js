a43e342fb2f5cf8be2af5bcc2d835e728f5b322c
joaquin <joacodicio@gmail.com>
2023-04-24 23:09:50 -0300

    proyecto casi final

--- js/main.js
+++ js/main.js
@@ -10,3 +10,17 @@ function checkScroll(){
         navbar.classList.remove('on-scroll')
     }
 }
+
+function setGallery(){
+    const images = document.querySelectorAll('.gallery-item')
+    images.forEach((img)=>{
+        const newLike = document.createElement('i')
+        newLike.className='fa-regular fa-heart like-icon'
+        newLike.addEventListener('click',(e)=>handleLike(e))
+        img.appendChild(newLike)
+    })
+}
+
+function handleLike(e){
+    e.target.classList.toggle('liked')
+}
